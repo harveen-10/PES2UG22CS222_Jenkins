72ce70c25fe878ee87282f3a0c2b71c29885c1c0
harveen-10 <harveenkaur100@gmail.com>
2025-03-12 18:46:26 +0530

    Added another cout command

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Working with Jenkins! \n";
+  cout << "Automatically getting updated when pushed to github! \n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
